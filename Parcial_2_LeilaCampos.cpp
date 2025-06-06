ed68d53ffc723c269244fbf9b087ad51975cd835
camposleila <00141525@uca.edu.sv>
2025-06-06 13:50:35 -0600

    Prueba

---
+++ Parcial_2_LeilaCampos.cpp
@@ -0,0 +1,12 @@
+//Ejercicio 2. Pide el puntaje de un jugador (1-5 estrellas) y muestra un mensaje acorde a su desempeño
+
+#include <iostream>
+using namespace std; 
+
+int main (){
+    int a, b, c, d, e; 
+
+    
+
+    return 0; 
+}
