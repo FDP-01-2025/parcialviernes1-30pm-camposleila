9f56d32b64d033ba0cfba7b13fed0ea57a876cab
camposleila <00141525@uca.edu.sv>
2025-06-06 14:39:23 -0600

    update ej 1

---
+++ EJERCICIO1ARRAYS.cpp
@@ -0,0 +1,18 @@
+// Dado un array, cuenta cuantos elementos aparecen más de una vez.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int numero;
+    cout << "inserta los 5 numeros para contar los que se repiten" << endl;
+    cin >> numero;
+
+    for (int i = 0; i >= 5; i++)
+    {
+        
+        cout << i << "se repite" << endl;
+    }
+    return 0;
+}
