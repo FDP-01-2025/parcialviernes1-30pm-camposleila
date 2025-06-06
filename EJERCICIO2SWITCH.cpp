628efde4ef4104002b2f2592d7042d452701b269
camposleila <00141525@uca.edu.sv>
2025-06-06 14:22:48 -0600

    ejercicio 2 switch

---
+++ EJERCICIO2SWITCH.cpp
@@ -0,0 +1,39 @@
+// Ejercicio 2. Pide el puntaje de un jugador (1-5 estrellas) y muestra un mensaje acorde a su desempeño
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int score;
+    cout << "What was your score? (1-5 stars)" << endl;
+    cin >> score; 
+
+    switch (score)
+    {
+    case 1:
+        {cout << "You only got 1 star. Keep trying" << endl;
+        break;}
+
+    case 2:
+        cout << "You got 2 stars. Not bad at all" << endl;
+        break;
+    
+    case 3: 
+        cout <<"3 stars!! Keep the rythm:)" << endl; 
+        break; 
+
+    case 4: 
+        cout << "Almost perfect score. Great" << endl; 
+        break;
+
+    case 5: 
+        cout << "Excellent!! You nailed it:)" << endl; 
+        break;
+
+    default:
+        cout << "Insert a valid answer" << endl; 
+    }
+
+    return 0;
+}
