2ee42d372e9bd97bc70d7e362c78cfb3e568e35d
camposleila <00141525@uca.edu.sv>
2025-06-06 13:57:45 -0600

    prueba2

--- Parcial_2_LeilaCampos.cpp
+++ Parcial_2_LeilaCampos.cpp
@@ -1,12 +1,25 @@
-//Ejercicio 2. Pide el puntaje de un jugador (1-5 estrellas) y muestra un mensaje acorde a su desempeño
+// Ejercicio 2. Pide el puntaje de un jugador (1-5 estrellas) y muestra un mensaje acorde a su desempeño
 
 #include <iostream>
-using namespace std; 
+using namespace std;
 
-int main (){
-    int a, b, c, d, e; 
+int main()
+{
+    int score;
+    cout << "What was your score? (1-5 stars)" << endl;
+    cin >> score;
+    switch (score)
+    {
+    case 1:
+        cout << "You only got 1 star. Keep trying" << endl;
+        break;
 
-    
+    case 2:
+        cout << "You got 2 stars. Not bad at all" << endl;
+        break;
 
-    return 0; 
+    default:
+    }
+
+    return 0;
 }
