c7a784503b5b19b240989e97bc38a902533ec049
camposleila <00141525@uca.edu.sv>
2025-06-06 15:39:27 -0600

    update

--- EJERCICIO1ARRAYS.cpp
+++ EJERCICIO1ARRAYS.cpp
@@ -5,18 +5,19 @@ using namespace std;
 
 int main()
 {
-    int numero, suma, i;
-    
-    numero = 0 ; 
+    int numero, suma, i; //declarar variables
 
-    cout << "inserta los 5 numeros para contar los que se repiten" << endl;
+    cout << "inserta los 5 numeros para contar los que se repiten" << endl; 
 
-    for (int i = 1; i <= 5; i++)
+    for (int i = 1; i <= 5; i++) //Para que el usuario ingrese los numeros (en este caso 5) 
     {
         cout << "Numero " << i << ": ";
         cin >> numero;
     }
-    cout << numero << " se repite " << i << "veces" << endl;
-
+    if (numero = numero) 
+    {
+        cout << numero << " se repite " << i << "veces" << endl;
+    }
+    
     return 0;
 }
